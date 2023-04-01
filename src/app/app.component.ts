6cb75bcbf3a20fe978138fcdf1f5dc1e2be0fcfd
ProsESportu <t.hajduga@poczta.fm>
2023-04-01 18:06:49 +0200

    deez

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, collectionData } from "@angular/fire/firestore";
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Timetable';
+
+
+  constructor(){
+
+  }
+}
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { TableCellComponent } from './table-cell/table-cell.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TableCellComponent
+  ],
+  imports: [
+    BrowserModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    BrowserAnimationsModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
